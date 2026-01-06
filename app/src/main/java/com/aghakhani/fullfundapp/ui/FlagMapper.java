139ad32d4d41ec78669229ad0067840f27838dea
Kiarash Aghakhani <kiarash88ca@gmail.com>
2026-01-06 18:37:47 -0400

    Adding country flags

--- app/src/main/java/com/aghakhani/fullfundapp/ui/FlagMapper.java
+++ app/src/main/java/com/aghakhani/fullfundapp/ui/FlagMapper.java
@@ -14,6 +14,27 @@ public final class FlagMapper {
         if (countryCode == null) return R.drawable.flag_placeholder;
 
         switch (countryCode.trim().toUpperCase()) {
+            case "SE":
+                return R.drawable.flag_sweden_simple;
+            case "NL":
+                return R.drawable.flag_netherlands_simple;
+            case "HU":
+                return R.drawable.flag_hungary_simple;
+            case "FR":
+                return R.drawable.flag_france_simple;
+            case "BE":
+                return R.drawable.flag_belgium_simple;
+            case "AE":
+                return R.drawable.flag_uae_simple;
+            case "CN":
+                return R.drawable.flag_china_simple;
+            case "TR":
+                return R.drawable.flag_turkey_simple;
+            case "NZ":
+                return R.drawable.flag_newzealand_simple;
+            case "SA":
+                return R.drawable.flag_saudi_simple;
+
             case "CA": return R.drawable.flag_canada_simple;
             case "US": return R.drawable.flag_usa_simple;
             case "UK":
